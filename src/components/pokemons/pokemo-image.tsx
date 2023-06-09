8942bd4a9a8ad2d6e735318fb0efa0aa6d0a0ed5
JesusGMM <jesus.g.m30@hotmail.com>
2023-06-09 15:39:53 -0500

    App de pokemon con qwik

---
+++ src/components/pokemons/pokemo-image.tsx
@@ -0,0 +1,31 @@
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
+
+export const PokemonImage = component$((props: { id: number; image: boolean; mostrar?: boolean }) => {
+	const cargando = useSignal(true);
+
+	useTask$(({ track }) => {
+		track(() => props.id);
+		cargando.value = true;
+	});
+
+	return (
+		<div
+			class='bg-purple-600 rounded mt-3 flex items-center justify-center'
+			style={{
+				height: '96px',
+				width: '96px',
+			}}
+		>
+			{cargando.value && <span>Cargando...</span>}
+			<img
+				class={['object-cover transition-all', { hidden: cargando.value, 'brightness-0': props.mostrar }]}
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/
+				${(props.image ? 'back/' : '') + props.id}.png`}
+				alt='Pokemon'
+				height={96}
+				width={96}
+				onLoad$={() => (cargando.value = false)}
+			/>
+		</div>
+	);
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,49 @@
-import { component$ } from '@builder.io/qwik';
+import { $, component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+import { PokemonImage } from '../components/pokemons/pokemo-image';
 
 export default component$(() => {
-  return (
-    <>
-      <Hero />
-      <Starter />
+	// useSignal VALORES PRIMIVOS
+	const pokemonId = useSignal(1);
+	const voltear = useSignal(false);
+	const mostrar = useSignal(false);
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
+	const buscarPokemon = $((valor: number) => {
+		if (pokemonId.value + valor <= 0) return;
+		pokemonId.value += valor;
+	});
 
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
+	return (
+		<>
+			<span class='text-2xl'>Buscador simple</span>
+			<span class='text-7xl'>{pokemonId}</span>
 
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
+			<PokemonImage id={pokemonId.value} image={voltear.value} mostrar={mostrar.value} />
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{' '}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-    </>
-  );
+			<div class='mt-3'>
+				<button class='btn btn-primary mr-2' onClick$={() => buscarPokemon(-1)}>
+					Anterior
+				</button>
+				<button class='btn btn-primary mr-2' onClick$={() => buscarPokemon(1)}>
+					Siguiente
+				</button>
+				<button class='btn btn-primary mr-2' onClick$={() => (voltear.value = !voltear.value)}>
+					Voltear
+				</button>
+				<button class='btn btn-primary' onClick$={() => (mostrar.value = !mostrar.value)}>
+					{mostrar.value ? 'Mostrar' : 'Ocultar'}
+				</button>
+			</div>
+		</>
+	);
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
+	title: 'Pokemon Qwik',
+	meta: [
+		{
+			name: 'Jesus MArtinez',
+			content: 'Esta es pokemon con qwick',
+		},
+	],
 };
